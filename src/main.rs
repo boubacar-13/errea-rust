9855aa676e2db171e0bd419627ba9e8f2d7fced8
Boubacar <timera.boubacar@gmail.com>
2024-06-23 23:36:37 +0200

    Green window

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-mod map;
-mod robot;
-mod station;
+extern crate piston;
+extern crate piston_window;
+extern crate find_folder;
 
-use map::Map;
-use robot::{Robot, RobotType};
-use robot::explorer::Explorer;
-use robot::collector::Collector;
-use robot::chemical_analyzer::ChemicalAnalyzer;
-use station::Station;
+use piston::event_loop::{Events, EventSettings, EventLoop};
+use piston::input::{RenderEvent, UpdateEvent};
+use piston_window::{PistonWindow, WindowSettings, clear};
+use find_folder::Search;
 
-fn main() {
-    let mut map = Map::new(100, 100, 42);
-    let mut station = Station::new();
+pub struct App {
+    // Add fields if needed for the state of the app
+}
 
-    let mut explorer_robot = Robot::new(RobotType::Explorer, Box::new(Explorer));
-    let mut collector_robot = Robot::new(RobotType::Collector, Box::new(Collector));
-    let mut chemical_robot = Robot::new(RobotType::ChemicalAnalyzer, Box::new(ChemicalAnalyzer));
+impl App {
+    fn render(&mut self, window: &mut PistonWindow, event: &piston_window::Event) {
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 
-    for _ in 0..100 {
-        explorer_robot.execute(&mut map, &mut station);
-        collector_robot.execute(&mut map, &mut station);
-        chemical_robot.execute(&mut map, &mut station);
-        station.resolve_conflicts();
-        station.create_robot_if_needed();
-        map.display();
+        window.draw_2d(event, |c, g, _| {
+            clear(GREEN, g);
+        });
+    }
+
+    fn update(&mut self) {
+        // Update the app's state here
     }
 }
 
+fn main() {
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App {
+        // Initialize fields if needed
+    };
+
+    let mut events = Events::new(EventSettings::new()).ups(60);
+
+    while let Some(e) = events.next(&mut window) {
+        if let Some(_) = e.render_args() {
+            app.render(&mut window, &e);
+        }
+
+        if let Some(_) = e.update_args() {
+            app.update();
+        }
+    }
+}
